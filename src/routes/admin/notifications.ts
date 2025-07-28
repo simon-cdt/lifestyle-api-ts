18d348d9d3314f9528685bb75fa280c0a1e78a95
simon-cdt <simon.cdt@eduge.ch>
2025-07-28 20:09:43 +0200

    admin notif

--- src/routes/admin/notifications.ts
+++ src/routes/admin/notifications.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 const expo = new Expo();
 
-router.post("/", async (req, res) => {
+router.post("/send", async (req, res) => {
   const { userIds, title, body, userId } = req.body;
 
   try {
@@ -20,6 +20,7 @@ router.post("/", async (req, res) => {
     }
 
     const users = await prisma.user.findMany({
+      where: { id: { in: userIds } },
       select: { id: true, pushToken: true },
     });
 
@@ -67,6 +68,48 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error("Erreur lors de l'envoi des notifications :", error);
     res.status(500).json({ message: "Erreur interne du serveur." });
+    return;
+  }
+});
+
+router.post("/users", async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, role: true },
+    });
+    if (!user || user.role !== "ADMIN") {
+      res
+        .status(404)
+        .json({ message: "Utilisateur non autorisé.", success: false });
+      return;
+    }
+
+    const users = await prisma.user.findMany({
+      select: { id: true, firstName: true, lastName: true },
+    });
+
+    if (!users || users.length === 0) {
+      res
+        .status(400)
+        .json({ message: "Aucun utilisateurs trouvés.", success: false });
+      return;
+    }
+
+    const usersFactorised = users.map((user) => {
+      return {
+        id: user.id,
+        name: user.firstName + " " + user.lastName,
+      };
+    });
+
+    res.status(200).json({ success: true, users: usersFactorised });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Une erreur est survenue.", success: false });
     return;
   }
 });
